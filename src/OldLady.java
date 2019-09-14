9a81af801f634a64d7f341573fc9cce312e7d164
greiterruslan <greiterruslan@192.168.7.31>
2019-09-13 20:05:16 -0700

    OldLady.java

--- src/OldLady.java
+++ src/OldLady.java
@@ -4,6 +4,25 @@ public class OldLady {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		 System.out.println("  ______                 __");
+		 System.out.println("||      ||   ||        ||  \\\\");
+		 System.out.println("||      ||   ||        ||   \\\\");
+		 System.out.println("||      ||   ||        ||   ||");
+		 System.out.println("||      ||   ||        ||   // ");
+		 System.out.println("||______||   ||_____   ||__//");
+		 System.out.println("");
+		 System.out.println("");
+		 System.out.println("                         __     ");
+		 System.out.println("||           //\\\\      ||  \\\\    \\\\    // ");
+		 System.out.println("||          //  \\\\     ||   \\\\    \\\\  // ");
+		 System.out.println("||         //____\\\\    ||   ||     \\\\//   ");
+		 System.out.println("||        //      \\\\   ||   //      ||  ");
+		 System.out.println("||______ //        \\\\  ||__//       ||   ");
+		
+		  
+
+
+		 
 	}
 
 }
