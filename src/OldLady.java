f3aaed7423090ae5f8bbae9425ab5c936986d028
greiterruslan <greiterruslan@192.168.7.31>
2019-09-16 16:01:03 -0700

    OldLadyIsDone.java

--- src/OldLady.java
+++ src/OldLady.java
@@ -1,28 +1,81 @@
 
 public class OldLady {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		// TODO Auto-generated method stub
+		System.out.println("");
+		Oldlady();
+		System.out.println(" fly");
+		Swallowed();
+		System.out.println("");
+		Oldlady();
+		System.out.println(" spider,");
+		System.out.println("That wiggled and iggled and jiggleg inside her;");
+		SpiderToFly();
+		Swallowed();
+		System.out.println("");
+		Oldlady();
+		System.out.println(" bird;");
+		System.out.println("How absurd to swallow a bird.");
+		BirdToSpider();
+		SpiderToFly();
+		Swallowed();
+		System.out.println("");
+		Oldlady();
+		System.out.println(" cat;");
+		System.out.println("Fancy that to swallow a cat!");
+		CatToBird();
+		BirdToSpider();
+		SpiderToFly();
+		Swallowed();
+		System.out.println("");
+		Oldlady();
+		System.out.println(" dog;");
+		System.out.println("What a hog, to swallow a dog;");
+		System.out.println("She swallowed the dog to catch the cat,");
+		CatToBird();
+		BirdToSpider();
+		SpiderToFly();
+		Swallowed();
+		System.out.println("");
 
-		 System.out.println("  ______                 __");
-		 System.out.println("||      ||   ||        ||  \\\\");
-		 System.out.println("||      ||   ||        ||   \\\\");
-		 System.out.println("||      ||   ||        ||   ||");
-		 System.out.println("||      ||   ||        ||   // ");
-		 System.out.println("||______||   ||_____   ||__//");
-		 System.out.println("");
-		 System.out.println("");
-		 System.out.println("                         __     ");
-		 System.out.println("||           //\\\\      ||  \\\\    \\\\    // ");
-		 System.out.println("||          //  \\\\     ||   \\\\    \\\\  // ");
-		 System.out.println("||         //____\\\\    ||   ||     \\\\//   ");
-		 System.out.println("||        //      \\\\   ||   //      ||  ");
-		 System.out.println("||______ //        \\\\  ||__//       ||   ");
+		System.out.println("There was an old lady who swallowed a horse...");
+		System.out.println("She's dead, of course!");
+
 		
-		  
-
-
-		 
 	}
+		
+		
+		public static void Swallowed() {
+		System.out.println("I don't know why she swallowed a fly - ");
+		System.out.println("Perhaps she'll die!");
+		
+		}
+		
+		public static void Fly() {
+		System.out.println(" fly");
+		
+		}
+		
+		public static void Oldlady() {
+		System.out.print("There was an old lady who swallowed a");
+		
+		}
+	
+		public static void SpiderToFly() {
+			System.out.print("She swallowed the spider to catch the");
+			Fly();
+		}
+		public static void BirdToSpider() {
+		System.out.println("She swallowed the bird to catch the spider,");
+		}
+		public static void CatToBird() {
+		System.out.println("She swallowed the cat to catch the bird,");
+	
+		}
+		
 
 }
+
+
+
